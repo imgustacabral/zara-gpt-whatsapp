b63f006dfbb49bc9347b604ac2f4bb04462c2151
imgustacabral <imgustacabral@gmail.com>
2023-04-02 22:18:08 -0300

    refactor: router.service

---
+++ src/message/dto/command.dto.ts
@@ -0,0 +1,6 @@
+import WAWebJS from 'whatsapp-web.js';
+
+export class CommandDto {
+  msg: WAWebJS.Message;
+  to: string;
+}
--- src/message/router/router.service.ts
+++ src/message/router/router.service.ts
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { Injectable } from '@nestjs/common';
 import { MessageService } from '../message.service';
 import { Client, LocalAuth, MessageMedia } from 'whatsapp-web.js';
 import * as qrcode from 'qrcode';
 import { ReplyMessageDto } from '../dto/reply-message.dto';
 import { getCannotGenerateImageError } from '../helpers/error-messages';
+import { CommandDto } from '../dto/command.dto';
 
 @Injectable()
 export class RouterService {
   client: Client;
+  commands: Map<string, Function>;
+
   constructor(private readonly messageService: MessageService) {
     this.client = new Client({
       authStrategy: new LocalAuth({ clientId: 'zara-bot' }),
@@ -16,6 +20,16 @@ export class RouterService {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     });
+
+    this.commands = new Map<string, Function>();
+    this.commands.set('/imagine', this.handleImagineCommand.bind(this));
+    this.commands.set('/clear', this.handleClearCommand.bind(this));
+    this.commands.set('contribuir', this.handleDonateCommand.bind(this));
+    this.commands.set('/doar', this.handleDonateCommand.bind(this));
+    this.commands.set('/donate', this.handleDonateCommand.bind(this));
+    this.commands.set('doar', this.handleDonateCommand.bind(this));
+    this.commands.set('/help', this.handleHelpCommand.bind(this));
+    this.commands.set('ajuda', this.handleHelpCommand.bind(this));
   }
 
   initialize(): void {
@@ -39,47 +53,12 @@ export class RouterService {
       const command = msg.body.toLowerCase().split(' ')[0];
       const to = msg.from;
       const isGroupMessage = msg.from.split('@')[1] === 'g.us';
-      console.log(isGroupMessage);
-      console.log(command);
-      if (command === '/imagine') {
-        const createdImage = await this.messageService.createImage(msg.body);
-        if (createdImage === 400) {
-          return await this.reply({
-            to,
-            body: getCannotGenerateImageError(),
-          });
-        }
-        return await this.replyImage({
-          to,
-          body: createdImage,
-        });
+
+      const handler = this.commands.get(command);
+      if (handler) {
+        return await handler(msg, to);
       }
 
-      if (command === '/clear') {
-        return await this.reply({
-          body: await this.messageService.clearMessageHistory(msg.from),
-          to: msg.from,
-        });
-      }
-
-      if (
-        command === 'contribuir' ||
-        command === '/doar' ||
-        command === '/donate' ||
-        command === 'doar'
-      ) {
-        return await this.reply({
-          to,
-          body: await this.messageService.donationMessage(),
-        });
-      }
-
-      if (command === '/help' || command === 'ajuda') {
-        return await this.reply({
-          to,
-          body: await this.messageService.helpMessage(),
-        });
-      }
       if (isGroupMessage && command !== 'zara') {
         return;
       }
@@ -122,5 +101,42 @@ export class RouterService {
     const media = await MessageMedia.fromUrl(replyMessageDto.body);
 
     return await this.client.sendMessage(replyMessageDto.to, media);
+  }
+
+  async handleImagineCommand(commandDto: CommandDto) {
+    const createdImage = await this.messageService.createImage(
+      commandDto.msg.body,
+    );
+    if (createdImage === 400) {
+      return await this.reply({
+        to: commandDto.msg.to,
+        body: getCannotGenerateImageError(),
+      });
+    }
+    return await this.replyImage({
+      to: commandDto.msg.to,
+      body: createdImage,
+    });
+  }
+
+  async handleClearCommand(commandDto: CommandDto) {
+    return await this.reply({
+      body: await this.messageService.clearMessageHistory(commandDto.msg.from),
+      to: commandDto.msg.from,
+    });
+  }
+
+  async handleDonateCommand(commandDto: CommandDto) {
+    return await this.reply({
+      to: commandDto.msg.to,
+      body: await this.messageService.donationMessage(),
+    });
+  }
+
+  async handleHelpCommand(commandDto: CommandDto) {
+    return await this.reply({
+      to: commandDto.msg.to,
+      body: await this.messageService.helpMessage(),
+    });
   }
 }
